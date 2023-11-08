e68b2d9615a25baa831285b2761925082e4dce5e
Pachi-Dev <pachi.claros@gmail.com>
2023-11-07 18:47:57 -0600

    Add new files and make updates to existing files for Agrotech website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()]
+export default defineConfig({  
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "es",
+        locales: {
+          en: "en",
+          es: "es",
+        },
+      },
+    }),
+    tailwind(),
+    astroI18next(),
+  ],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/flowbite/**/*.js'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'agro-green': '#8BBF20',
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
