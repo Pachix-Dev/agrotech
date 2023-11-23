bdf6a187afde58dd84fa9fae525ecb85c190c586
Pachix-Dev <pachi.claros@gmail.com>
2023-11-22 22:47:06 -0600

    Update image paths and add lazy loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
-
 import partytown from "@astrojs/partytown";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hfmexico.mx/agrotechmexico/",
   base: "/agrotechmexico",
-  publicDir: "/agrotechmexico/public",  
+  packageOptions: {
+    // Set type to module for ECMAScript module support
+    type: 'module',
+    knownEntrypoints: ['@astrojs/partytown'],
+  },    
   integrations: [sitemap({
     i18n: {
       defaultLocale: "es",
